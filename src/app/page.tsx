2d99e95aaf75686204ade036bb7188c24cb191d2
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-11-04 16:15:20 +0530

    feat(app): add simple chat interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,46 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Card, CardContent } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+import { ArrowUp } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="bg-white min-h-screen w-screen">
+      <div className="max-w-3xl mx-auto flex flex-col space-y-10 mt-10 pb-40">
+        <p className="text-black/50 text-sm font-[450]">Hello!!</p>
+        <div className="flex flex-row space-x-2 -translate-x-6">
+          <Avatar className="size-6 mt-4">
+            <div className="bg-linear-to-br from-pink-500 to-yellow-500 h-8 w-8 rounded-full"></div>
+          </Avatar>
+          <Card>
+            <CardContent className="text-gray-600 text-sm font-[450]">
+              <p>Hello, how may I help you today?</p>
+            </CardContent>
+          </Card>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <p className="text-black/50 text-sm font-[450]">What is kimi k2</p>
+        <div className="flex flex-row space-x-2 -translate-x-6">
+          <Avatar className="size-6 mt-4">
+            <div className="bg-linear-to-br from-pink-500 to-yellow-500 h-8 w-8 rounded-full"></div>
+          </Avatar>
+          <Card>
+            <CardContent className="text-gray-600 text-sm font-[450]">
+              <p>Kimi K2 is an LLM that was released by Ollama in 2025. It was made to work originally with vulkan but now it works with cuda. Do you want to learn more?</p>
+            </CardContent>
+          </Card>
         </div>
-      </main>
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 max-w-[802px] mx-auto">
+        <div className="absolute bottom-full left-0 right-0 h-8 bg-linear-to-t from-white to-transparent pointer-events-none"></div>
+        
+        <div className="relative bg-white pt-1 pb-8">
+          <Textarea rows={2} className="resize-none text-sm bg-gray-50" placeholder="Type your message here..." />
+          <button className="absolute right-2 top-[30px] -translate-y-1/2 bg-linear-to-br from-pink-500 to-yellow-500 text-white placeholder:text-gray-100 p-2 rounded-full text-sm font-medium shadow-sm hover:opacity-90 transition duration-200 cursor-pointer">
+            <ArrowUp size={15}/>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
